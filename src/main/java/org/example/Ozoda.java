beb9bd28b4c66d4c9fb58d794665d59943fa13c9
ozodadevelop <ozodahasanova16@gmail.com>
2025-02-07 10:55:31 +0500

    this is a kline of cofde

--- src/main/java/org/example/Ozoda.java
+++ src/main/java/org/example/Ozoda.java
@@ -4,5 +4,7 @@ public class Ozoda {
       public void test(){
           System.out.println("test");
           System.out.println("Ozoda !");
+          System.out.println("Ozoda !");
+          System.out.println("Ozoda !");
       }
 }
