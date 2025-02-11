3ad5392b5694cf22ef9df2ef828f771e4198c00e
Dilnura <dilnurahamdamova001@gmail.com>
2025-02-11 20:07:36 +0500

    mmmm

--- src/main/java/org/example/Dilnura.java
+++ src/main/java/org/example/Dilnura.java
@@ -3,10 +3,5 @@ package org.example;
 public class Dilnura {
 public static void test(){
     System.out.println("Dilnura");
-    System.out.println("fgh");
-    System.out.println("gtghtr");
-    System.out.println("dsfsfg");
-    System.out.println("dsfsfg");
-    System.out.println("dsfsfg");
 }
 }
